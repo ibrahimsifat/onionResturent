698f8d7fc488d211a6fe85176a55d0d08a561fb7
ibrahimsifat <ibsifat24@gmail.com>
2021-10-15 20:26:49 +0300

    cardl-added

--- src/components/Home/Meals-container/Meals/Brackfast/AddtoCard/Checkoutbody.js
+++ src/components/Home/Meals-container/Meals/Brackfast/AddtoCard/Checkoutbody.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import CheckoutHeader from './CheckoutHeader';
 const Checkoutbody=(props)=>{
-    console.log(props.item);
+    const {title,Price,img}=props.item
     return(
-   <div className="container">
-       <CheckoutHeader></CheckoutHeader>
-       <div>
-    <h1>{props.item.title}</h1>
-        <img src={props.item.img} alt="" />
+   <div className="  mx-auto  h-screen-full py-16">
+       <div className='md:grid w-10/12 mx-auto grid-cols-2 justify-between items-center'>
+ <div className="left_site md:mr-16">
+     <h1 className='text-5xl md:font-semibold pb-6'>{title}</h1>
+     <h2 className='text-gray-500 pb-3'>
+     We all know breakfast is the most important meal of the day. But just because you're aware that this meal is crucial doesn't mean you always have time to whip up something delicious. From school drop-offs to busy commutes to squeezing in a sweat session, weekday mornings can often be our busiest time of day.
+     </h2>
+     <h2 className='text-4xl md:font-semibold pb-6'>${Price}</h2>
+     <button className='bg-red-500 text-white px-7 py-2 rounded-full'>Add</button>
+ </div>
+ <div className="right_site md:w-9/12">
+
+     <img src={img} alt="" />
+ </div>
     </div>
    </div>
     )
